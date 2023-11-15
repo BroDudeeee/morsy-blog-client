63214bc45e02e02d3ea0d9e119fc95a6b5aa577a
Ahmed Morsy <108017273+BroDudeeee@users.noreply.github.com>
2023-11-15 04:39:51 +0200

    Update SinglePost.jsx multer

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -2,12 +2,30 @@ import { Link } from "react-router-dom";
 import moment from "moment";
 import "./SinglePost.css";
 
-const SinglePost = ({ _id, title, body, createdAt, category, image }) => {
+const SinglePost = ({ _id, title, createdAt, category, image }) => {
+  function arrayBufferToBase64(buffer) {
+    let binary = "";
+    const bytes = new Uint8Array(buffer);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+  }
+
   return (
     <article className="singlePostContainer">
       <Link to={`post/${_id}`} className="link">
         <section className="singlePost">
-          {image && <img src={image} alt={title} className="postImg" />}
+          {image && (
+            <img
+              src={`data:${image.contentType};base64,${arrayBufferToBase64(
+                image.data.data
+              )}`}
+              alt={title}
+              className="postImg"
+            />
+          )}
           <section className="postTitleBody">
             {category && <span className="category">#{category}</span>}
             <h3 className="title">{title}</h3>
